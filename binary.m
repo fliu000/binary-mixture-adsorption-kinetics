11bd4dc56e7abaa4101fff703d4f81fb73984484
fliu000 <fangliuche@gmail.com>
2015-12-12 16:53:34 -0500

    Update binary.m

--- binary.m
+++ binary.m
@@ -2,11 +2,11 @@
 # author: fliu000
 # original version
 
-function binary(c1, c2, maxLoop)
+function binary_2(c1, c2, maxLoop)
     D   = 2.8 * 1e-6;
     x_m = 4.12 * 1e-10;  # saturation adsorption
     a1  = 2.9 * 1e-8;  # a1 is Langmuir von Szyskowski constant
-    a2  = 2.9 * 1e-6;  # a2 is Langmuir von Szyskowski constant
+    a2  = 2.97 * 1e-7;  # a2 is Langmuir von Szyskowski constant
     t   = 0.001;
     x   = (1 : maxLoop);
     y   = (1 : maxLoop);
@@ -18,8 +18,9 @@ function binary(c1, c2, maxLoop)
     y_2 = (1 : maxLoop);
     x_3 = (1 : maxLoop);
     y_3 = (1 : maxLoop);
+    time_axis = (1 : maxLoop);
     
-    maxIteration = 1000;
+    maxIteration = 100000;
     
     x(1) = 0;
     y(1) = 0;
@@ -27,15 +28,17 @@ function binary(c1, c2, maxLoop)
     c1_s(1) = a1 * x(1) / (x_m - x(1) - y(1));
     c2_s(1) = a2 * y(1) / (x_m - x(1) - y(1));
     
-    x(2) = 2 * c1 * sqrt(D * t / pi);
-    y(2) = 2 * c2 * sqrt(D * t / pi);
+    x(2) = 2 * c1 * sqrt(D * t^2 / pi);
+    y(2) = 2 * c2 * sqrt(D * t^2 / pi);
     
     c1_s(2) = a1 * x(2) / (x_m - x(2) - y(2));
     c2_s(2) = a2 * y(2) / (x_m - x(2) - y(2));
     
-
+    time_axis(1) = 0;
+    time_axis(2) = t^2;
     for n = 3 : maxLoop
-        tn = (t * (n-1))^2 ;
+        tn = (t * (n-1))^2;
+        time_axis(n) = tn;
         
         # Step 1: make the first estimate
         x_1(n) =  x(n-1) + (x(n-1) - x(n-2));
@@ -51,10 +54,10 @@ function binary(c1, c2, maxLoop)
             
             # Step 3: make the second estimate
             p1(n) = (sum (c1_s(n)) - c1_s(1) - c1_s(n)) * t; 
-            x_2(n) = 2 * sqrt(D / pi) * (c1 * sqrt(tn) - (1/2 * c1_s(n) * t + p1(n) + 1/2 * c1_s(1) * t));
+            x_2(n) = 2 * sqrt(D / pi) * (c1 * sqrt(tn) - (1/(n-1)^2 * c1_s(n) * t + p1(n) + ((n-1)^2-1)/(n-1)^2 * c1_s(1) * t));
             
             p2(n) = (sum (c2_s(n)) - c2_s(1) - c2_s(n)) * t; 
-            y_2(n) = 2 * sqrt(D / pi) * (c2 * sqrt(tn) - (1/2 * c2_s(n) * t + p2(n) + 1/2 * c2_s(1) * t));
+            y_2(n) = 2 * sqrt(D / pi) * (c2 * sqrt(tn) - (1/(n-1)^2 * c2_s(n) * t + p2(n) + ((n-1)^2-1)/(n-1)^2 * c2_s(1) * t));
             
             # Step 4: check whether second estimates are within 1%
             # of first estimate
@@ -65,7 +68,7 @@ function binary(c1, c2, maxLoop)
             #disp(x_1(n));
             #disp(abs(x_2(n)/x_1(n) - 1));
             
-            if (abs(x_2(n)/x_1(n) - 1) < 0.01 && abs(y_2(n)/y_1(n) - 1) < 0.01)
+            if (abs(x_2(n)/x_1(n) - 1) < 0.01 && abs(y_2(n)/y_1(n) - 1) < 0.01 && (x_m - x_2(n)) > 0 && (x_m - y_2(n)) > 0)
                 # If yes, use second estimate as x(n), y(n)
                 x(n) = x_2(n);
                 y(n) = y_2(n);
@@ -90,8 +93,6 @@ function binary(c1, c2, maxLoop)
     #disp(x_1);
     #disp("est 2.");
     #disp(x_2);
-
-    time_axis = 0 : t : (maxLoop-1) * t;    
 
     clf;
     plot(time_axis, x);
